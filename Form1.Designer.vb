fa31d302f7574ffddf8ca1d6f344414cc44040ec
John Paul Calverley <Jpcal1991@outlook.com>
2023-01-04 11:40:19 +0000

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,73 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class formLinearArray
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.btnInitiateArray = New System.Windows.Forms.Button()
+        Me.btnSearch = New System.Windows.Forms.Button()
+        Me.txtFind = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'btnInitiateArray
+        '
+        Me.btnInitiateArray.Location = New System.Drawing.Point(98, 12)
+        Me.btnInitiateArray.Name = "btnInitiateArray"
+        Me.btnInitiateArray.Size = New System.Drawing.Size(181, 45)
+        Me.btnInitiateArray.TabIndex = 0
+        Me.btnInitiateArray.Text = "Initialise array"
+        Me.btnInitiateArray.UseVisualStyleBackColor = True
+        '
+        'btnSearch
+        '
+        Me.btnSearch.Location = New System.Drawing.Point(98, 193)
+        Me.btnSearch.Name = "btnSearch"
+        Me.btnSearch.Size = New System.Drawing.Size(181, 45)
+        Me.btnSearch.TabIndex = 1
+        Me.btnSearch.Text = "Search"
+        Me.btnSearch.UseVisualStyleBackColor = True
+        '
+        'txtFind
+        '
+        Me.txtFind.Location = New System.Drawing.Point(98, 113)
+        Me.txtFind.Name = "txtFind"
+        Me.txtFind.Size = New System.Drawing.Size(181, 26)
+        Me.txtFind.TabIndex = 2
+        '
+        'formLinearArray
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(372, 252)
+        Me.Controls.Add(Me.txtFind)
+        Me.Controls.Add(Me.btnSearch)
+        Me.Controls.Add(Me.btnInitiateArray)
+        Me.Name = "formLinearArray"
+        Me.Text = "Linear Array Search"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnInitiateArray As Button
+    Friend WithEvents btnSearch As Button
+    Friend WithEvents txtFind As TextBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,45 @@
+﻿Public Class formLinearArray
+
+    Dim strNames() As String 'Declares the non static (dynamic) array for use
+
+    Private Sub btnInitiateArray_Click(sender As Object, e As EventArgs) Handles btnInitiateArray.Click
+
+        ReDim strNames(3)
+
+        strNames(0) = "John"
+        strNames(1) = "Mark"
+        strNames(2) = "Jill"
+        strNames(3) = "Jess"
+        MsgBox("Array populated!") 'Preset array values are created upon button press and display that they have been added
+
+    End Sub
+
+    Private Sub btnSearch_Click(sender As Object, e As EventArgs) Handles btnSearch.Click
+
+        Dim bFound As Boolean 'Declared to check if text input is in the array
+        Dim iReply As Integer ' Declared to check if the new nae should be added to the list
+
+        Dim strFind As String 'Declared to search for names in the array
+        strFind = Me.txtFind.Text
+
+        For iCount As Integer = 0 To 3
+            If UCase(strNames(iCount)) = UCase(strFind) Then 'Checks input in uppercase with uppercase array
+                bFound = True
+                Exit For
+            End If
+        Next 'Interating through each name in array to check for match
+
+        If bFound = True Then
+            MsgBox("Found " & strFind) 'Searches for a match
+        Else
+            iReply = MsgBox(strFind & " not found. Would you like to add them to the record?", MsgBoxStyle.YesNo) 'Displays message and yes no for name addition
+            If iReply = vbYes Then
+                ReDim Preserve strNames(UBound(strNames) + 1)
+                strNames(UBound(strNames)) = strFind
+                MsgBox(strFind & " has been added to the records.")
+            End If
+        End If 'If match is not found, prompted to add name into the record
+
+    End Sub
+    'John Calverley
+End Class
